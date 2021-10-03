21d351d9685ab2082f55d79a5e99cefad473c832
Khomdrad-Art <khomdradboon@gmail.com>
2021-10-03 12:46:09 +0700

    Update extra_policies.sh

--- prep/extra_policies.sh
+++ prep/extra_policies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #this script needs the following variables to be defined
-TL_USER=demoshiftleft
-TL_PASS=demoshiftleft
-TL_CONSOLE=52.253.83.186
+#TL_USER=demoshiftleft
+#TL_PASS=demoshiftleft
+#TL_CONSOLE=52.253.83.186
 
 #create evilpetclinic collection
 curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' -X POST \
